110698a48a2f44c9d4a81d18c2178c4ea1655c69
Pablo Vivera <pvivera@gmail.com>
2012-10-02 09:30:11 -0400

    Initial implementation

---
+++ TinyTimer/TinyTimer.Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TinyTimer.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TinyTimer.Benchmark(() =>
+                {
+                    var samples = new long[100000];
+                    for (var i = 0; i < samples.Length; i++)
+                    {
+                        samples[i] = new Random().Next(10);
+                    }
+                }));
+
+            Console.WriteLine(TinyTimer.Benchmark(() =>
+                {
+                    var result = 2 ^ 1000000;
+                }, 10000));
+
+            Console.Read();
+        }
+    }
+}
---
+++ TinyTimer/TinyTimer/TinyTimer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TinyTimer
+{
+    public class TinyTimer
+    {
+        public static double Benchmark(Action action, int samples = 10)
+        {
+            var times = new TimeSpan[samples];
+
+            for (var i = 0; i < samples; i++)
+            {
+                var start = DateTime.Now;
+                action.Invoke();
+                times[i] = DateTime.Now - start;
+            }
+
+            return TimeSpan.FromTicks(times.Select(t=>t.Ticks).Sum() / samples).TotalMilliseconds;
+        }
+    }
+}
